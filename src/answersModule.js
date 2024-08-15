e498cbef18fd5a3ceb6baeb066de0b4af605a491
Mohamed Abdallahi M'khaitir <msidi0415@gmail.com>
2024-08-15 12:35:20 +0000

    Ajout des fonctionnalité

--- src/answersModule.js
+++ src/answersModule.js
@@ -1,5 +1,64 @@
+// const { connectDB } = require('./config/database');
+
+// async function createAnswer(answer) {
+//     try {
+//         const db = await connectDB();
+//         const result = await db.collection('answers').insertOne(answer);
+//         console.log('Answer created:', result.insertedId);
+//         return result;
+//     } catch (err) {
+//         console.error('Error creating answer:', err.message);
+//     }
+// }
+
+// async function readAnswers() {
+//     try {
+//         const db = await connectDB();
+//         const answers = await db.collection('answers').find().toArray();
+//         console.log('Answers:', answers);
+//         return answers;
+//     } catch (err) {
+//         console.error('Error reading answers:', err.message);
+//     }
+// }
+
+// async function updateAnswer(answerId, update) {
+//     try {
+//         const db = await connectDB();
+//         const result = await db.collection('answers').updateOne(
+//             { answerId: answerId },
+//             { $set: update }
+//         );
+//         console.log('Number of documents modified:', result.modifiedCount);
+//         return result;
+//     } catch (err) {
+//         console.error('Error updating answer:', err.message);
+//     }
+// }
+
+// async function deleteAnswer(answerId) {
+//     try {
+//         const db = await connectDB();
+//         const result = await db.collection('answers').deleteOne({ answerId: answerId });
+//         console.log('Number of documents deleted:', result.deletedCount);
+//         return result;
+//     } catch (err) {
+//         console.error('Error deleting answer:', err.message);
+//     }
+// }
+
+// module.exports = { createAnswer, readAnswers, updateAnswer, deleteAnswer };
+
+
+
+
+
+
+
+
 const { connectDB } = require('./config/database');
 
+// Créer une nouvelle réponse
 async function createAnswer(answer) {
     try {
         const db = await connectDB();
@@ -11,17 +70,20 @@ async function createAnswer(answer) {
     }
 }
 
+// Lire toutes les réponses
 async function readAnswers() {
     try {
         const db = await connectDB();
         const answers = await db.collection('answers').find().toArray();
-        console.log('Answers:', answers);
+        // Utiliser JSON.stringify pour afficher les réponses de manière lisible
+        console.log('Answers:', JSON.stringify(answers, null, 2));
         return answers;
     } catch (err) {
         console.error('Error reading answers:', err.message);
     }
 }
 
+// Mettre à jour une réponse
 async function updateAnswer(answerId, update) {
     try {
         const db = await connectDB();
@@ -36,6 +98,7 @@ async function updateAnswer(answerId, update) {
     }
 }
 
+// Supprimer une réponse
 async function deleteAnswer(answerId) {
     try {
         const db = await connectDB();
@@ -48,3 +111,4 @@ async function deleteAnswer(answerId) {
 }
 
 module.exports = { createAnswer, readAnswers, updateAnswer, deleteAnswer };
+
--- src/questionsModule.js
+++ src/questionsModule.js
@@ -1,5 +1,61 @@
+// const { connectDB } = require('./config/database');
+
+// async function createQuestion(question) {
+//     try {
+//         const db = await connectDB();
+//         const result = await db.collection('questions').insertOne(question);
+//         console.log('Question created:', result.insertedId);
+//         return result;
+//     } catch (err) {
+//         console.error('Error creating question:', err.message);
+//     }
+// }
+
+// async function readQuestions() {
+//     try {
+//         const db = await connectDB();
+//         const questions = await db.collection('questions').find().toArray();
+//         console.log('Questions:', questions);
+//         return questions;
+//     } catch (err) {
+//         console.error('Error reading questions:', err.message);
+//     }
+// }
+
+// async function updateQuestion(questionId, update) {
+//     try {
+//         const db = await connectDB();
+//         const result = await db.collection('questions').updateOne(
+//             { questionId: questionId },
+//             { $set: update }
+//         );
+//         console.log('Number of documents modified:', result.modifiedCount);
+//         return result;
+//     } catch (err) {
+//         console.error('Error updating question:', err.message);
+//     }
+// }
+
+// async function deleteQuestion(questionId) {
+//     try {
+//         const db = await connectDB();
+//         const result = await db.collection('questions').deleteOne({ questionId: questionId });
+//         console.log('Number of documents deleted:', result.deletedCount);
+//         return result;
+//     } catch (err) {
+//         console.error('Error deleting question:', err.message);
+//     }
+// }
+
+// module.exports = { createQuestion, readQuestions, updateQuestion, deleteQuestion };
+
+
+
+
+
 const { connectDB } = require('./config/database');
 
+// Créer une nouvelle question
 async function createQuestion(question) {
     try {
         const db = await connectDB();
@@ -11,17 +67,20 @@ async function createQuestion(question) {
     }
 }
 
+// Lire toutes les questions
 async function readQuestions() {
     try {
         const db = await connectDB();
         const questions = await db.collection('questions').find().toArray();
-        console.log('Questions:', questions);
+        // Utiliser JSON.stringify pour afficher les questions de manière lisible
+        console.log('Questions:', JSON.stringify(questions, null, 2));
         return questions;
     } catch (err) {
         console.error('Error reading questions:', err.message);
     }
 }
 
+// Mettre à jour une question
 async function updateQuestion(questionId, update) {
     try {
         const db = await connectDB();
@@ -36,6 +95,7 @@ async function updateQuestion(questionId, update) {
     }
 }
 
+// Supprimer une question
 async function deleteQuestion(questionId) {
     try {
         const db = await connectDB();
@@ -48,3 +108,4 @@ async function deleteQuestion(questionId) {
 }
 
 module.exports = { createQuestion, readQuestions, updateQuestion, deleteQuestion };
+
